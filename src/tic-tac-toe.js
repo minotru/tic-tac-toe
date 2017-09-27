04b9553dac0d7143dbcfc8cbacadacc9989814ec
Simon <senich10@mail.ru>
2017-09-27 19:04:33 +0300

    Initial commit.

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,59 @@
 class TicTacToe {
     constructor() {
+        this.grid = [];
+        for (let i = 0; i < 3; i++) {
+            this.grid[i] = [];
+            for (let j = 0; j < 3; j++)
+                this.grid[i][j] = null;
+        }
+        this.currentPlayer = 'x';
+    }
 
+    checkWinOf(symbol) {
+        let g = this.grid, s = symbol;
+        return ((g[0][0] == s && g[0][1] == s && g[0][2] == s) ||
+                (g[1][0] == s && g[1][1] == s && g[1][2] == s) ||
+                (g[2][0] == s && g[2][1] == s && g[2][2] == s) ||
+                (g[0][0] == s && g[1][1] == s && g[2][2] == s) ||
+                (g[0][2] == s && g[1][1] == s && g[2][0] == s));
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        this.grid[rowIndex][columnIndex] = this.currentPlayer;
+        this.currentPlayer = (this.currentPlayer == 'x') ? 'o': 'x';
     }
 
     isFinished() {
-
+        return this.getWinner() != null || this.isDraw(); 
     }
 
     getWinner() {
-
+        if (this.checkWinOf('x'))
+            return 'x';
+        else if (this.checkWinOf("o"))
+            return 'o';
+        else
+            return null;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++)
+                if (this.grid[i][j])
+                    return false;
+        return true;
     }
 
     isDraw() {
-
+        return this.noMoreTurns() && !this.getWinner();
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.grid[rowIndex][colIndex];
     }
 }
 
